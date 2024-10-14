337b3cee42f7e40b20908bbbd61ea0917b055163
chuvitvang <152381244+chuvitvang@users.noreply.github.com>
2024-10-14 16:03:54 +0700

    Update bai2.R

--- bai2.R
+++ bai2.R
@@ -1,22 +1,17 @@
 library(ggplot2)
+library(dplyr)
 
-# Sử dụng dataframe diamonds có sẵn
-df <- diamonds
-
-# Lọc dữ liệu chỉ lấy màu D và J
-df_filtered <- df %>%
-  filter(color %in% c("D", "J"))
-
-# Tính tổng số lượng cho mỗi tổ hợp cut và color
-df_summary <- aggregate(carat ~ cut + color, df_filtered, FUN = length)
-names(df_summary)[3] <- "count"
+# Sử dụng dataframe diamonds có sẵn và lọc dữ liệu
+df_summary <- diamonds %>%
+  filter(color %in% c("D", "J")) %>%
+  count(cut, color, name = "count")
 
 # Vẽ biểu đồ cột đôi
 ggplot(data = df_summary, aes(x = cut, y = count, fill = color)) +
-  geom_col(color = "navy",position = "dodge") +
+  geom_col(color = "navy", position = "dodge") +
   geom_text(aes(label = count), position = position_dodge(width = 0.9), vjust = -0.5) +
   theme_bw() +
-  labs(x = "Cut", y = "Count", fill = "Coulor") +
+  labs(x = "Cut", y = "Count", fill = "Color") +
   theme(
     panel.grid.minor = element_blank(),
     panel.grid.major = element_blank(),
@@ -25,5 +20,5 @@ ggplot(data = df_summary, aes(x = cut, y = count, fill = color)) +
     plot.title = element_text(hjust = 0.5),
     axis.ticks = element_line(colour = "red")
   ) +
-  scale_fill_manual(values = c("D" = "#00FFFF", "J" = "#FFCCFF"))+
+  scale_fill_manual(values = c("D" = "#00FFFF", "J" = "#FFCCFF")) +
   scale_y_continuous(limits = c(0, 3000))
